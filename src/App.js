edbe58f4ed9dae6c96f9a6a00c81ca36b7890503
Murilo Benatti <benatti1104@gmail.com>
2023-01-10 23:19:29 -0300

    Update completed buttom

--- src/App.js
+++ src/App.js
@@ -5,14 +5,19 @@ import TodoList from './components/TodoList';
 
 function App() {
   const [inputText, setInputText] = useState (""); 
+    const [todos, setTodos] = useState ([]); 
      return (
     <div className="App">
      
      <header>
         <h1>Murilo Todo List</h1>
 </header>
-    <Form setInputText={setInputText} />
-    <TodoList />
+    <Form
+     inputText={inputText}
+      todos={todos} 
+      setTodos={setTodos }
+       setInputText={setInputText} />
+    <TodoList setTodos={setTodos} todos={todos}/>
     </div>
   );
 }
